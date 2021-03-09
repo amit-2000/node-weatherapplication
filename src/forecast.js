3c24190e4833150d3bff9499eb3a9a3a458e4a18
amit-2000 <58249832+amit-2000@users.noreply.github.com>
2021-03-09 18:33:04 +0530

    Create forecast.js

--- src/forecast.js
+++ src/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lattitude, longitude, callback) => {
             callback('unable to find location', undefined)
 
         } else {
-            callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degress and feels like ${body.current.feelslike} degrees. `)
+            callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degress & feels like ${body.current.feelslike} degrees. `)
 
         }
     })
